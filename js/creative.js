238de0dde42c3cd17fbed571fcefce3671cc9c1b
unknown <lidianemara93@gmail.com>
2019-07-29 17:49:28 -0300

    add video testimonial

--- js/creative.js
+++ js/creative.js
@@ -92,6 +92,18 @@ $('#inline-popups').magnificPopup({
   midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 });
 
+$(".video-play").on('click', function(e) {
+  e.preventDefault(); 
+  var vidWrap = $(this).parent(),
+      iframe = vidWrap.find('.video iframe'),
+      iframeSrc = iframe.attr('src'),
+      iframePlay = iframeSrc += "?autoplay=1";
+  vidWrap.children('.video-thumbnail').fadeOut();
+  vidWrap.children('.video-play').fadeOut();
+  vidWrap.find('.video iframe').attr('src', iframePlay);
+
+
+});
 
 
 
